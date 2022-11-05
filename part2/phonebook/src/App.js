61a12c4ffc077edc03adc97048183b9f473b6094
6vllz <luisitm2000@gmail.com>
2022-11-05 19:21:53 -0300

    phonebook finished

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,21 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 
 const App = () => {
-  const [allPersons, setAllPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
-
+  const [allPersons, setAllPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newFilter, setNewFilter] = useState("");
-  const [filterPersons, setFilterPersons] = useState(allPersons);
+  const [filterPersons, setFilterPersons] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setAllPersons(response.data);
+      setFilterPersons(response.data);
+    });
+  }, []);
 
   const alreadyExist = (personName) => {
     const names = allPersons.map((person) => person.name);
